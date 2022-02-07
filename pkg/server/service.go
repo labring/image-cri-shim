b6bed0b29ffc1158dd7c6dae74309d07cc6038a8
cuisongliu <cuisongliu@qq.com>
2022-02-07 12:51:55 +0800

    add force bool

--- pkg/server/service.go
+++ pkg/server/service.go
@@ -17,6 +17,8 @@ const (
 
 func (s *server) ListImages(ctx context.Context,
 	req *api.ListImagesRequest) (*api.ListImagesResponse, error) {
+	req.Filter.Image.Image = s.replaceImage(req.Filter.Image.Image, "ListImages")
+
 	rsp, err := (*s.imageService).ListImages(ctx, req)
 
 	if err != nil {
@@ -28,6 +30,8 @@ func (s *server) ListImages(ctx context.Context,
 
 func (s *server) ImageStatus(ctx context.Context,
 	req *api.ImageStatusRequest) (*api.ImageStatusResponse, error) {
+
+	req.Image.Image = s.replaceImage(req.Image.Image, "ImageStatus")
 	rsp, err := (*s.imageService).ImageStatus(ctx, req)
 
 	if err != nil {
@@ -39,18 +43,7 @@ func (s *server) ImageStatus(ctx context.Context,
 
 func (s *server) PullImage(ctx context.Context,
 	req *api.PullImageRequest) (*api.PullImageResponse, error) {
-	// TODO we can change the image name of req, and make the cri pull the image we need.
-	// for example:
-	// req.Image.Image = "sealer.hub/library/nginx:1.1.1"
-	// and the cri will pull "sealer.hub/library/nginx:1.1.1", and save it as "sealer.hub/library/nginx:1.1.1"
-	// note:
-	// but kubelet sometimes will invoke imageService.RemoveImage() or something else. The req.Image.Image will the original name.
-	// so we'd better tag "sealer.hub/library/nginx:1.1.1" with original name "req.Image.Image" After "rsp, err := (*s.imageService).PullImage(ctx, req)".
-	domain, named := splitDockerDomain(req.Image.Image)
-	if domain != "" {
-		req.Image.Image = SealosHub + "/" + named
-	}
-	klog.Infof("pulling image name is: %s", req.Image.Image)
+	req.Image.Image = s.replaceImage(req.Image.Image, "PullImage")
 	rsp, err := (*s.imageService).PullImage(ctx, req)
 
 	if err != nil {
@@ -62,6 +55,7 @@ func (s *server) PullImage(ctx context.Context,
 
 func (s *server) RemoveImage(ctx context.Context,
 	req *api.RemoveImageRequest) (*api.RemoveImageResponse, error) {
+	req.Image.Image = s.replaceImage(req.Image.Image, "RemoveImage")
 	rsp, err := (*s.imageService).RemoveImage(ctx, req)
 
 	if err != nil {
@@ -81,7 +75,21 @@ func (s *server) ImageFsInfo(ctx context.Context,
 
 	return rsp, err
 }
-
+func (s *server) replaceImage(image, action string) string {
+	// TODO we can change the image name of req, and make the cri pull the image we need.
+	// for example:
+	// req.Image.Image = "sealer.hub/library/nginx:1.1.1"
+	// and the cri will pull "sealer.hub/library/nginx:1.1.1", and save it as "sealer.hub/library/nginx:1.1.1"
+	// note:
+	// but kubelet sometimes will invoke imageService.RemoveImage() or something else. The req.Image.Image will the original name.
+	// so we'd better tag "sealer.hub/library/nginx:1.1.1" with original name "req.Image.Image" After "rsp, err := (*s.imageService).PullImage(ctx, req)".
+	domain, named := splitDockerDomain(image)
+	if domain != "" {
+		image = SealosHub + "/" + named
+	}
+	klog.Infof("image name is: %s , action: %s", image, action)
+	return image
+}
 func splitDockerDomain(name string) (domain, remainder string) {
 	i := strings.IndexRune(name, '/')
 	if i == -1 || (!strings.ContainsAny(name[:i], ".:") && name[:i] != "localhost" && strings.ToLower(name[:i]) == name[:i]) {
